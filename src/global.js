8e0c998f4c16cb50185e59066df0471772040bd5
ram <rammalahi7301@gmail.com>
2025-08-13 17:49:07 +0530

    final

--- src/global.js
+++ src/global.js
@@ -1,8 +1,8 @@
 
 import { createSlice } from "@reduxjs/toolkit";
 
-var APIurl ="https://uaam.beyondexs.com/api/"
-var baseurl ="https://uaam.beyondexs.com/"
+// var APIurl ="https://uaam.beyondexs.com/api/"
+// var baseurl ="https://uaam.beyondexs.com/"
 // // var APIurl ="https://dvmtapi.bexatm.com/uaam/api/"
 // // var baseurl ="https://dvmtapi.bexatm.com/uaam/api/"
 // var APIurl ="https://phpmigrationapi.bexatm.com/uaam/api/"
