6b9aead5eb0c75e603423f8d732fb666adeaac76
jayakavitha <sjayakavitha2002@gmail.com>
2025-08-19 11:11:19 +0530

    file

--- src/ui-components/global/Autocomplete.jsx
+++ src/ui-components/global/Autocomplete.jsx
@@ -467,6 +467,76 @@ export const SingleFormikOptimizedAutocompleteName_CodeListListView = ({
     />
   );
 };
+export const CheckinAutocomplete = ({
+    value = null,
+    onChange,
+    url,
+    height = 20,
+    defaultValue,
+    ...props
+  }) => {
+    const [options, setOptions] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        if (!url) return;
+        setLoading(true);
+        try {
+          const response = await axios.get(url, {
+            headers: {
+              Authorization: "eyJhbGciOiJIUzI1NiIsInR5cGUiOiJKV1QifQ.eyJzdWIiOiJCZXhAMTIzIiwibmFtZSI6IkJleCIsImFkbWluIjp0cnVlLCJleHAiOjE2Njk5ODQzNDl9.uxE3r3X4lqV_WKrRKRPXd-Jub9BnVcCXqCtLL4I0fpU",
+            },
+          });
+          const data = response.data.Data.rows || [];
+          setOptions(data);
+        } catch (err) {
+          setOptions([]);
+        } finally {
+          setLoading(false);
+        }
+      };
+  
+      fetchData();
+    }, [url]);
+
+    return (
+      <Autocomplete
+        size="small"
+        fullWidth
+        limitTags={1}
+        options={options}
+        loading={loading}
+        value={value}
+        isOptionEqualToValue={(option, value) => option.Name === value.Name}
+        onChange={(event, newValue) => onChange(newValue)}
+        getOptionLabel={(option) => option.Name}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label={props.label || "Select Options"}
+            // error={!!error}
+            // helperText={error}
+
+            {...props}
+            variant="standard"
+            focused
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <>
+                  {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                  {params.InputProps.endAdornment}
+                </>
+              ),
+            }}
+          />
+        )}
+        {...props}
+      />
+    );
+  };
 export const SingleFormikOptimizedAutocompleteName_CodeListListView_Product = ({
   data,
   value = null,
