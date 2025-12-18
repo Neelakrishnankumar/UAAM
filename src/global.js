715b6a31058324ea66d5c9e2cf76817a49166dad
Manoj <manojkumark1617@gmail.com>
2025-12-18 11:36:42 +0530

    manoj

--- src/global.js
+++ src/global.js
@@ -1,11 +1,11 @@
 
 import { createSlice } from "@reduxjs/toolkit";
 
-var APIurl ="https://uaam.beyondexs.com/api/"
-var baseurl ="https://uaam.beyondexs.com/"
+// var APIurl ="https://uaam.beyondexs.com/api/"
+// var baseurl ="https://uaam.beyondexs.com/"
 
-//  var APIurl ="https://uaamuat.beyondexs.com/api/"
-//  var baseurl ="https://uaamuat.beyondexs.com/"
+ var APIurl ="https://uaamuat.beyondexs.com/api/"
+ var baseurl ="https://uaamuat.beyondexs.com/"
 
 // var APIurl ="https://dvmtapi.bexatm.com/uaam/api/"
 // var baseurl ="https://dvmtapi.bexatm.com/uaam/api/"
