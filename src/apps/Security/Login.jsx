7919ea5fb50f4c15a63181fa58b28041742f0cf8
Manoj <manojkumark1617@gmail.com>
2026-02-28 16:35:14 +0530

    manoj

--- src/apps/Security/Login.jsx
+++ src/apps/Security/Login.jsx
@@ -421,7 +421,8 @@ import { fetchApidata } from "../../store/reducers/LoginReducer";
 import { toast } from "react-hot-toast";
 import { fetchComboData1 } from "../../store/reducers/Comboreducer";
 // import background from "../../assets/img/Back_Office_Final.png";
-import background from "../../assets/img/UAAM_CoverImage.png";
+// import background from "../../assets/img/UAAM_CoverImage.png";
+import background from "../../assets/img/UAAM_Cover1.png";
 import * as Yup from "yup";
 const FlexBox = styled(Box)(() => ({
   display: "flex",
@@ -661,7 +662,7 @@ const Login = () => {
         sx={{
           width: "100%",
           boxShadow: { xs: "none", sm: 3 },
-          borderRadius: { xs: 0, sm: 3 },
+          borderRadius: { xs: 0, sm: 0 },
 
           // ✅ Add this
           backgroundImage: {
@@ -680,7 +681,7 @@ const Login = () => {
           height: "100vh",
         }}>
 
-          <Grid item sm={8} xs={12}>
+          <Grid item sm={7} xs={12}>
 
             <ContentBox
               sx={{
@@ -689,9 +690,13 @@ const Login = () => {
                 // maxWidth: { xs: "100%", sm: 500 },
                 // px: { xs: 3, sm: 8 },
                 // py: { xs: 4, sm: 8 },
-                maxWidth: { xs: "95%", sm: 500 },
-                px: { xs: 2, sm: 6 },
-                py: { xs: 4, sm: 6 },
+                maxWidth: { xs: "95%", sm: 420 },
+                // px: { xs: 2, sm: 6 },
+                // py: { xs: 4, sm: 6 },
+                 paddingRight: { xs: 2, sm: 6 },
+                paddingLeft: { xs: 1, sm: 6 },
+                paddingTop: { xs: 0, sm: 0 },
+                paddingBottom: { xs: 0, sm: 0 },
                 backgroundColor: "#ffffff",
                 // ✅ Transparent on xs so background shows
                 backgroundColor: {
@@ -699,20 +704,20 @@ const Login = () => {
                   sm: "#ffffff",
                 },
                 backdropFilter: {
-                  xs: "blur(10px)",
+                  xs: "blur(15px)",
                   sm: "none",
                 },
               }}
             >
 
               {/* Header */}
-              <Box mb={5}>
+              <Box mb={3}>
                 <Typography
-                  variant="h4"
+                  variant="h6"
                   sx={{
-                    fontSize: { xs: "30px", sm: "40px" },
+                    fontSize: { xs: "20px", sm: "30px" },
                     fontWeight: 600,
-                    mb: 0.5,
+                    // mb: 0.5,
 
                     // background: "linear-gradient(180deg, rgba(10,64,99,1) 53%, rgba(6,128,150,1) 100%)",
                     background: "linear-gradient(180deg,rgba(10, 64, 99, 1) 37%, rgba(6, 128, 150, 1) 100%)",
@@ -749,7 +754,7 @@ const Login = () => {
                   <form onSubmit={handleSubmit}>
 
                     <Stack
-                      spacing={3}
+                      spacing={2}
                     >
 
                       <StyledTextField
@@ -790,7 +795,7 @@ const Login = () => {
                         InputProps={{
                           startAdornment: (
                             <InputAdornment position="start">
-                              <Box sx={{ color: "#999", fontSize: "18px" }}>🔒</Box>
+                              <Box sx={{ color: "#999", fontSize: "17px" }}>🔒</Box>
                             </InputAdornment>
                           ),
                           endAdornment: (
@@ -800,6 +805,10 @@ const Login = () => {
                               </IconButton>
                             </InputAdornment>
                           ),
+                           sx: {
+                            paddingRight: "14px",
+                            paddingLeft: "7px",
+                          },
                         }}
                         label="Password"
                       />
@@ -809,7 +818,7 @@ const Login = () => {
                           display: "flex",
                           justifyContent: "space-between",
                           alignItems: "center",
-                          mt: 1,
+                          // mt: 1,
                         }}
                       >
                         <FormControlLabel
@@ -868,7 +877,7 @@ const Login = () => {
            {/* Right Side: Image */}
           <Grid
             item
-            sm={4}
+            sm={5}
             xs={false}
             sx={{
               display: { xs: "none", sm: "flex" },
